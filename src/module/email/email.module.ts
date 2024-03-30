94c26694f395811807a95f1238e30c0883d36d76
daijja <buiminh8690@gmail.com>
2024-03-30 15:28:17 +0700

    feature/sendmail

--- src/module/email/email.module.ts
+++ src/module/email/email.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import { join } from 'path';
 
 @Module({
+  imports: [
+    MailerModule.forRoot({
+      // transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+      transport: {
+        host: 'smtp.gmail.com',
+        port: 587,
+        ignoreTLS: false,
+        secure: false,
+        auth: {
+          user: "buiminh8690@gmail.com",
+          pass: "xggnbvkvnepcxlni",
+        }
+      },
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new EjsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
   controllers: [EmailController],
   providers: [EmailService],
+  exports: [EmailService]
 })
-export class EmailModule {}
+export class EmailModule { }
